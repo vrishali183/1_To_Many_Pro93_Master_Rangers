0f208ff4e794b3f2fbfcca18051f9bce9e938420
vrishali183 <vrishali183@gmail.com>
2023-02-14 05:11:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function new_image(get_image)
 {
 		fabric.Image.fromURL(get_image, function(Img) {
 		block_image_object = Img;
-		block_image_object.scaleToWidth(block_image_width);
-		block_image_object.scaleToHeight(block_image_height);
+		block_image_object.scaleToWidth();//block_image_width
+		block_image_object.scaleToHeight();//block_image_height
 		block_image_object.set({top:block_y,left:block_x});
 		canvas.add(block_image_object);
 		});
@@ -26,32 +26,32 @@ function my_keydown(e)
 keyPressed = e.keyCode;
 console.log(keyPressed);
 
-	if(keyPressed == '82') 
+	if() //keyPressed == '82'
 	{
 		block_x = 10;
 		new_image('red.jpg');
 		console.log('R')
 	}
-	if(keyPressed == '71')
+	if()//keyPressed == '71'
 	{
 		block_x = 310;
 		new_image('green.png');
 		console.log('G')
 	}
 	
-	if(keyPressed == '89')
+	if()//keyPressed == '89'
 	{
 		block_x = 560;
 		new_image('yellow.png');
 		console.log('Y')
 	}
-	if(keyPressed == '80')
+	if()//keyPressed == '80'
 	{
 		block_x = 880;
 		new_image('pink.png');
 		console.log('P')
 	}
-	if(keyPressed == '66')
+	if()//keyPressed == '66'
 	{
 		block_x = 1040;
 		new_image('blue.png');
